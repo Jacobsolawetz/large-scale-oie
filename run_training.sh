db70f2ba562af73cb01092baa2b7712b00b719d8
Jacobsolawetz <jacobsolawetz@gmail.com>
2019-02-24 12:37:38 -0600

    add large_scale training data

--- run_training.sh
+++ run_training.sh
@@ -1,1 +1,1 @@
-allennlp train experiments/oie_labeler.json -s ~/Desktop/PHD/NLP/large-scale-oie/results/classic_fullrun --include-package large_scale_oie
+allennlp train experiments/oie_labeler.json -s ~/Desktop/PHD/NLP/large-scale-oie/results/classic_train --include-package large_scale_oie
