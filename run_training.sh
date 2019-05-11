5805220033d793d8f2e52ab9c25f1b870658e151
Jacobsolawetz <jacobsolawetz@gmail.com>
2019-05-11 18:11:36 -0500

    adding crf output layer

--- run_training.sh
+++ run_training.sh
@@ -1,1 +1,1 @@
-allennlp train experiments/oie_labeler.json -s ~/Desktop/PHD/NLP/large-scale-oie/results/classic_train --include-package large_scale_oie
+allennlp train experiments/oie_labeler_crf.json -s ~/Desktop/PHD/NLP/large-scale-oie/results/classic_train_crf --include-package large_scale_oie
